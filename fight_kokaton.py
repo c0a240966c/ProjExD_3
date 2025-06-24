197ef4cb0d3cb773305093690005f80964ab9483
Ryosei <c0a240966c@edu.teu.ac.jp>
2025-06-24 14:37:09 +0900

    練習2:爆弾衝突

--- fight_kokaton.py
+++ fight_kokaton.py
@@ -156,33 +156,32 @@ def main():
                 return
             if event.type == pg.KEYDOWN and event.key == pg.K_SPACE:
                 # スペースキー押下でBeamクラスのインスタンス生成
-                if beam is None:
-                    beam = Beam(bird)            
+                beam = Beam(bird)            
         screen.blit(bg_img, [0, 0])
-        
+
         if bomb is not None:
             if bird.rct.colliderect(bomb.rct):
-                # ゲームオーバー時に，こうかとん画像を切り替え，1秒間表示させる
                 bird.change_img(8, screen)
                 pg.display.update()
                 time.sleep(1)
                 return
 
-            if bomb is not None:
-                if beam is not None:
-                    if beam.rct.colliderect(bomb.rct):
-                        beam=None
-                        bomb=None
+        if beam is not None:
+            if beam is not None:
+                if beam.rct.colliderect(bomb.rct):
+                        beam = None
+                        bomb = None
+             
+        key_lst = pg.key.get_pressed()
+        bird.update(key_lst, screen)
 
-                key_lst = pg.key.get_pressed()
-                bird.update(key_lst, screen)
-                if beam is not None:
-                    beam.update(screen) 
-                if bomb is not None:  
-                    bomb.update(screen)
-                pg.display.update()
-                tmr += 1
-                clock.tick(50)
+        if beam is not None:
+                beam.update(screen) 
+        if bomb is not None:  
+                bomb.update(screen)
+        pg.display.update()
+        tmr += 1
+        clock.tick(50)
 
 
 if __name__ == "__main__":
